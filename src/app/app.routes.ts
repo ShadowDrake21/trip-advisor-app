b45338433576a8468e0b9abc412c7844c08f72e7
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-08-04 08:51:22 +0300

    favicons, initial files, some images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./pages/search/search.component').then((c) => c.SearchComponent),
+  },
+  {
+    path: 'location/:id',
+    loadComponent: () =>
+      import('./pages/location/location.component').then(
+        (c) => c.LocationComponent
+      ),
+  },
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+  },
+  { path: '**', redirectTo: '/not-found' },
+];
